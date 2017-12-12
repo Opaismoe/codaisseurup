73930b4fbee89746d759debbd8a04b1ce40e9bf8
opaismoe <wiebe.buursink@gmail.com>
2017-12-12 13:03:42 +0100

    Created models, Created validation

---
+++ app/models/event.rb
@@ -0,0 +1,7 @@
+class Event < ApplicationRecord
+  belongs_to :user
+
+
+  validates :name, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create!(name:"Coffee Party", description:"Somethinghere", user_id: 1)
