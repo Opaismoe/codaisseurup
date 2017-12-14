b60eae11613139d98067427fe574ca63833d8568
opaismoe <wiebe.buursink@gmail.com>
2017-12-14 22:03:00 +0100

    Add validate for Start_at End_at

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,13 @@
 class Event < ApplicationRecord
-  EVENT_CAPACITY = ["1","2","3","4","5","6","7","8","9"]
+  EVENT_CAPACITY = (1..25).to_a
   BARGAIN = 40
 
   belongs_to :user
   has_and_belongs_to_many :categories
   has_many :photos, dependent: :destroy
 
+  validates :starts_at, :ends_at, :presence => true
+  validate :end_after_start
 
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -22,6 +24,12 @@ class Event < ApplicationRecord
 
   def self.order_by_price
     order :price
+  end
+
+  def end_after_start
+    if ends_at < starts_at
+      errors.add(:ends_at, "must be after the start date")
+    end
   end
 
 
